49303edb5d4ad122a10e610048e8c842c7a6e99c
ChuQuocVu <chuquocvu1234@gmail.com>
2022-12-11 18:16:54 +0700

    Remove testFunc button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -71,7 +71,6 @@ namespace Schedule_Management
             this.label9 = new System.Windows.Forms.Label();
             this.textBoxTable = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridViewStatus = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -433,16 +432,6 @@ namespace Schedule_Management
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "SQL Server Connect";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(405, 461);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 34;
-            this.button1.Text = "TestFunction";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // dataGridViewStatus
             // 
             this.dataGridViewStatus.AllowUserToAddRows = false;
@@ -504,7 +493,6 @@ namespace Schedule_Management
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1485, 748);
             this.Controls.Add(this.dataGridViewStatus);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.groupBox3);
@@ -558,7 +546,6 @@ namespace Schedule_Management
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox textBoxTable;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
