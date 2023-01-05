70379a76e4339351c46e7abee5843f16106bdd63
ChuQuocVu <chuquocvu1234@gmail.com>
2023-01-06 01:25:09 +0700

    [Filter window] Add filter pop up window when click btnFilter

---
+++ Filter.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Schedule_Management
+{
+    partial class Filter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_TimeIn = new System.Windows.Forms.TextBox();
+            this.txt_TImeOut = new System.Windows.Forms.TextBox();
+            this.txt_Status = new System.Windows.Forms.TextBox();
+            this.txt_FilterName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Filter = new System.Windows.Forms.Button();
+            this.datePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txt_TimeIn
+            // 
+            this.txt_TimeIn.Location = new System.Drawing.Point(45, 123);
+            this.txt_TimeIn.Name = "txt_TimeIn";
+            this.txt_TimeIn.Size = new System.Drawing.Size(118, 22);
+            this.txt_TimeIn.TabIndex = 0;
+            this.txt_TimeIn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_TImeOut
+            // 
+            this.txt_TImeOut.Location = new System.Drawing.Point(169, 123);
+            this.txt_TImeOut.Name = "txt_TImeOut";
+            this.txt_TImeOut.Size = new System.Drawing.Size(118, 22);
+            this.txt_TImeOut.TabIndex = 1;
+            this.txt_TImeOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_Status
+            // 
+            this.txt_Status.Location = new System.Drawing.Point(293, 123);
+            this.txt_Status.Name = "txt_Status";
+            this.txt_Status.Size = new System.Drawing.Size(118, 22);
+            this.txt_Status.TabIndex = 2;
+            this.txt_Status.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_FilterName
+            // 
+            this.txt_FilterName.Location = new System.Drawing.Point(42, 41);
+            this.txt_FilterName.Multiline = true;
+            this.txt_FilterName.Name = "txt_FilterName";
+            this.txt_FilterName.Size = new System.Drawing.Size(262, 40);
+            this.txt_FilterName.TabIndex = 3;
+            this.txt_FilterName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Student\'s name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(42, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Time in";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(169, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Time out";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Status";
+            // 
+            // btn_Filter
+            // 
+            this.btn_Filter.Location = new System.Drawing.Point(317, 41);
+            this.btn_Filter.Name = "btn_Filter";
+            this.btn_Filter.Size = new System.Drawing.Size(115, 40);
+            this.btn_Filter.TabIndex = 11;
+            this.btn_Filter.Text = "Search";
+            this.btn_Filter.UseVisualStyleBackColor = true;
+            this.btn_Filter.Click += new System.EventHandler(this.btn_Filter_Click);
+            // 
+            // datePicker1
+            // 
+            this.datePicker1.CustomFormat = "MM/dd/yyyy";
+            this.datePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datePicker1.Location = new System.Drawing.Point(183, 11);
+            this.datePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.datePicker1.Name = "datePicker1";
+            this.datePicker1.Size = new System.Drawing.Size(121, 22);
+            this.datePicker1.TabIndex = 55;
+            this.datePicker1.ValueChanged += new System.EventHandler(this.datePicker1_ValueChanged);
+            // 
+            // Filter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(469, 181);
+            this.Controls.Add(this.datePicker1);
+            this.Controls.Add(this.btn_Filter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_FilterName);
+            this.Controls.Add(this.txt_Status);
+            this.Controls.Add(this.txt_TImeOut);
+            this.Controls.Add(this.txt_TimeIn);
+            this.Name = "Filter";
+            this.Text = "Filter";
+            this.Load += new System.EventHandler(this.Filter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_TimeIn;
+        private System.Windows.Forms.TextBox txt_TImeOut;
+        private System.Windows.Forms.TextBox txt_Status;
+        private System.Windows.Forms.TextBox txt_FilterName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Filter;
+        private System.Windows.Forms.DateTimePicker datePicker1;
+    }
+}
---
+++ Filter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schedule_Management
+{
+    public partial class Filter : Form
+    {
+        public Filter()
+        {
+            InitializeComponent();
+        }
+
+        private void Filter_Load(object sender, EventArgs e)
+        {
+            txt_TimeIn.Text = "-";
+            txt_TImeOut.Text = "-";
+            txt_Status.Text = "-";
+            txt_FilterName.Text = "";
+        }
+
+        private void btn_Filter_Click(object sender, EventArgs e)
+        {
+            loadStudentActitvites();
+        }
+
+        private void loadStudentActitvites()
+        {
+            bool isFirst = true;
+            bool isName = false;
+            txt_TimeIn.Text = "-";
+            txt_TImeOut.Text = "-";
+            txt_Status.Text = "-";
+            string date = datePicker1.Text;
+
+            if (txt_FilterName.Text == "")
+            {
+                AutoClosingMessageBox_Filter.Show("Name is empty!", "", 1500);
+                return;
+            }
+
+            Form1 mainWindow = new Form1();
+
+            for (int r = 0; r < mainWindow.dataGridViewData.Rows.Count; r++)
+            {
+                if (mainWindow.dataGridViewData.Rows[r].Cells[1].Value.ToString().Trim().Substring(0, 9) == date)
+                {
+                    if (mainWindow.dataGridViewData.Rows[r].Cells[2].Value.ToString().Trim() == txt_FilterName.Text)
+                    {
+                        isName = true;
+                        if (isFirst)
+                        {
+                            txt_TimeIn.Text = mainWindow.dataGridViewData.Rows[r].Cells[1].Value.ToString().Trim().Substring(0, 9);
+                            txt_Status.Text = mainWindow.dataGridViewData.Rows[r].Cells[4].Value.ToString().Trim();
+                            isFirst = false;
+                        }
+                        else
+                        {
+                            txt_TImeOut.Text = mainWindow.dataGridViewData.Rows[r].Cells[1].Value.ToString().Trim().Substring(0, 9);
+                        }
+                    }
+                }
+            }
+            if (!isName)
+            {
+                AutoClosingMessageBox_Filter.Show($"Can't find '{txt_FilterName.Text}' in system's history!", "", 1500);
+            }
+        }
+
+        private void datePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            loadStudentActitvites();
+        }
+    }
+
+    public class AutoClosingMessageBox_Filter
+    {
+        System.Threading.Timer _timeoutTimer;
+        string _caption;
+        AutoClosingMessageBox_Filter(string text, string caption, int timeout)
+        {
+            _caption = caption;
+            _timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                null, timeout, System.Threading.Timeout.Infinite);
+            using (_timeoutTimer)
+                MessageBox.Show(text, caption);
+        }
+        public static void Show(string text, string caption, int timeout)
+        {
+            new AutoClosingMessageBox_Filter(text, caption, timeout);
+        }
+        void OnTimerElapsed(object state)
+        {
+            IntPtr mbWnd = FindWindow("#32770", _caption); // lpClassName is #32770 for MessageBox
+            if (mbWnd != IntPtr.Zero)
+                SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            _timeoutTimer.Dispose();
+        }
+        const int WM_CLOSE = 0x0010;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+    }
+}
